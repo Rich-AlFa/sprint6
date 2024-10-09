3b62abe22e3e269854a190a15a3741f660348201
Rich-AlFa <odracir.alfa@gmail.com>
2024-10-08 19:50:43 -0600

    Change database location

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly_express as px
 
 car_data = pd.read_csv(
-    r'C:\Users\Usuario\Documents\GitHub\sprint6\vehicles_us.csv')
+    r'vehicles_us.csv')
 
 st.header('Aplicación para visualizar vehículos en venta')
 
